647f80ab311eb7388c3b0e570386117b68b34871
Duolasa <trynity.5@hotmail.com>
2014-05-23 18:17:25 +0100

    started adding coms

---
+++ PH SDK e AASMA PH/proj hoshimi/ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/ComHybrid/ComHybridAI.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using AASMAHoshimi;
+using AASMAHoshimi.Examples;
+using PH.Common;
+using AASMAHoshimi.BDI;   ///remove this after no longer needed
+using AASMAHoshimi.Hybrid;
+
+namespace AASMAHoshimi.ComHybrid
+{
+  public class ComHybridAI : AASMAAI
+  {
+    public ComHybridAI(NanoAI nano)
+    {
+      this._nanoAI = nano;
+    }
+
+    List<Point> HoshimiPoints = new List<Point>();
+    List<Point> OccupiedHoshimiPoints = new List<Point>();
+    List<Point> closePierres = new List<Point>();
+    List<Point> AZNpoints = new List<Point>();
+    List<Point> Navpoints = new List<Point>();
+
+    List<PlanCheckPoint> PlanCheckPointList = new List<PlanCheckPoint>();
+    PlanCheckPoint currentInstruction;
+    bool planIsFinished = true;
+    bool previousInstructionIsFinished = true;
+    enum Desire
+    {
+      None,
+      BuildContainer,
+      BuildProtector,
+      BuildExplorer,
+      BuildNeedle
+    };
+
+    Desire goal = Desire.None;
+
+    public override void DoActions()
+    {
+      CheckPerceptions();
+      if (!ReactiveLayer())
+      {
+        if (planIsFinished)
+        {
+          Deliberate();
+          Plan();
+          Execute();
+        }
+        else
+        {
+          Execute();
+        }
+
+        if (goal == Desire.None)
+        {
+          MoveRandomly();
+        }
+      }
+    }
+
+    private void CheckPerceptions()
+    {
+      AASMAPlayer framework = this.getAASMAFramework();
+      List<Point> visibleFullNeedles = framework.visibleFullNeedles(getNanoBot());
+      List<Point> visibleEmptyNeedles = framework.visibleEmptyNeedles(getNanoBot());
+      List<Point> visibleHoshimiPoints = framework.visibleHoshimies(getNanoBot());
+      List<Point> visibleAZNPoints = framework.visibleAznPoints(getNanoBot());
+      List<Point> visibleNavPoints = framework.visibleNavigationPoints(getNanoBot());
+
+      foreach (Point p in visibleHoshimiPoints)
+      {
+        if (visibleFullNeedles.Contains(p) || visibleEmptyNeedles.Contains(p))
+        {
+          if (!OccupiedHoshimiPoints.Contains(p))
+          {
+            OccupiedHoshimiPoints.Add(p);
+          }
+          if (HoshimiPoints.Contains(p))
+          {
+            HoshimiPoints.Remove(p);
+          }
+        }
+        else if (!HoshimiPoints.Contains(p) && !OccupiedHoshimiPoints.Contains(p))
+        {
+          HoshimiPoints.Add(p);         
+        }
+      }
+
+      foreach (Point p in visibleAZNPoints)
+      {
+        if (!AZNpoints.Contains(p))
+        {
+          AZNpoints.Add(p);
+
+          AASMAMessage msg = new AASMAMessage(this.getNanoBot().InternalName, "AZNPoint");
+          msg.Tag = p;
+          getAASMAFramework().broadCastMessage(msg);
+        }
+      }
+
+      foreach (Point p in visibleNavPoints)
+      {
+        if (!visibleNavPoints.Contains(p))
+        {
+          visibleNavPoints.Add(p);
+          AASMAMessage msg = new AASMAMessage(this.getNanoBot().InternalName, "NavPoint");
+          msg.Tag = p;
+          getAASMAFramework().broadCastMessage(msg);
+        }
+      }
+
+      List<Point> visiblePierres = framework.visiblePierres(getNanoBot());
+      closePierres.Clear();
+    }
+
+    private bool ReactiveLayer()
+    {
+
+      if (closePierres.Count > 0)
+      {
+        Point closestEnemy = Utils.getNearestPoint(getNanoBot().Location, closePierres);
+        int awayVectorX = getNanoBot().Location.X - closestEnemy.X;
+        int awayVectorY = getNanoBot().Location.Y - closestEnemy.Y;
+
+        Point awayFromPierre = new Point(getNanoBot().Location.X + awayVectorX / 2, getNanoBot().Location.Y + awayVectorY / 2);
+        getNanoBot().MoveTo(awayFromPierre);
+
+        return true;
+      }
+
+
+
+      if (getAASMAFramework().protectorsAlive() < 10)
+      {
+        this._nanoAI.Build(typeof(HybridProtector), "P" + this._protectorNumber++);
+        return true;
+      }
+      if (getAASMAFramework().containersAlive() < 10)
+      {
+        this._nanoAI.Build(typeof(HybridContainer), "C" + this._containerNumber++);
+        return true;
+      }
+      if (getAASMAFramework().explorersAlive() < 10)
+      {
+        this._nanoAI.Build(typeof(BDIExplorer), "E" + this._explorerNumber++);
+        return true;
+      }
+
+
+      return false;
+
+    }
+
+    private void Deliberate()
+    {
+      if (HoshimiPoints.Count > 0)
+      {
+        goal = Desire.BuildNeedle;
+        return;
+      }
+
+      goal = Desire.None;
+    }
+
+    private void Plan()
+    {
+      PlanCheckPointList.Clear();
+      previousInstructionIsFinished = true;
+      switch (goal)
+      {
+        case Desire.None:
+          PlanCheckPointList.Add(new PlanCheckPoint(getNanoBot().Location, PlanCheckPoint.Actions.MoveRandom));
+          break;
+        case Desire.BuildNeedle:
+          Point nearest = Utils.getNearestPoint(getNanoBot().Location, HoshimiPoints);
+          PlanCheckPointList.Add(new PlanCheckPoint(nearest, PlanCheckPoint.Actions.Move));
+          PlanCheckPointList.Add(new PlanCheckPoint(nearest, PlanCheckPoint.Actions.BuildNeedle));
+          planIsFinished = false;
+          break;
+      }
+    }
+
+    private void Execute()
+    {
+      if (previousInstructionIsFinished)
+      {
+        if (PlanCheckPointList.Count == 0)
+        {
+          planIsFinished = true;
+          return;
+        }
+        currentInstruction = PlanCheckPointList[0];
+        PlanCheckPointList.RemoveAt(0);
+      }
+
+
+      switch (currentInstruction.action)
+      {
+        case PlanCheckPoint.Actions.Move:
+          if (previousInstructionIsFinished)
+          {
+            getNanoBot().MoveTo(currentInstruction.location);
+            previousInstructionIsFinished = false;
+          }
+          else
+          {
+            if (getNanoBot().Location == currentInstruction.location)
+            {
+              previousInstructionIsFinished = true;
+            }
+          }
+          break;
+
+        case PlanCheckPoint.Actions.MoveRandom:
+          if (previousInstructionIsFinished)
+          {
+            MoveRandomly();
+          }
+          else
+          {
+            previousInstructionIsFinished = true;
+          }
+          break;
+
+        case PlanCheckPoint.Actions.BuildNeedle:
+          if (previousInstructionIsFinished)
+          {
+            this._nanoAI.Build(typeof(BDINeedle), "N" + this._needleNumber++);
+            OccupiedHoshimiPoints.Add(currentInstruction.location);
+            HoshimiPoints.Remove(currentInstruction.location);
+
+            AASMAMessage msg = new AASMAMessage(this.getNanoBot().InternalName, "EmptyNeedle");
+            msg.Tag = currentInstruction.location;
+            getAASMAFramework().broadCastMessage(msg);
+          }
+          else
+          {
+            if (OccupiedHoshimiPoints.Contains(currentInstruction.location))
+            {
+              planIsFinished = true;
+            }
+            else
+            {
+              previousInstructionIsFinished = true;
+            }
+          }
+          break;
+      }
+    }
+
+    private void MoveRandomly()
+    {
+      if (frontClear())
+      {
+        if (Utils.randomValue(100) < 80)
+        {
+          this.MoveForward();
+        }
+        else
+        {
+          this.RandomTurn();
+        }
+      }
+      else
+      {
+        this.RandomTurn();
+      }
+    }
+
+    public override void receiveMessage(AASMAMessage msg)
+    {
+      string content = msg.Content;
+      Point p = (Point) msg.Tag;
+
+      if (content.Equals("HoshimiPoint"))
+      {
+        if (!OccupiedHoshimiPoints.Contains(p) && !HoshimiPoints.Contains(p))
+        {
+          HoshimiPoints.Add(p);
+        } 
+      }
+
+      if (content.Equals("AZNPoint"))
+      {
+        if (!AZNpoints.Contains(p))
+        {
+          AZNpoints.Add(p);
+
+          AASMAMessage broadcastmsg = new AASMAMessage(this.getNanoBot().InternalName, "AZNPoint");
+          msg.Tag = p;
+          getAASMAFramework().broadCastMessage(broadcastmsg);
+        }
+      }
+
+      if (content.Equals("NavPoint"))
+      {
+        if (!Navpoints.Contains(p))
+        {
+          Navpoints.Add(p);
+
+          AASMAMessage broadcastmsg = new AASMAMessage(this.getNanoBot().InternalName, "NavPoint");
+          msg.Tag = p;
+          getAASMAFramework().broadCastMessage(broadcastmsg);
+        }
+      }
+    }
+  }
+}
---
+++ PH SDK e AASMA PH/proj hoshimi/ProjectHoshimiAASMA_v1_1_1/AASMAPlayer/ComHybrid/ComHybridContainer.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using PH.Common;
+using AASMAHoshimi;
+
+namespace AASMAHoshimi.ComHybrid
+{
+
+  [Characteristics(ContainerCapacity = 50, CollectTransfertSpeed = 5, Scan = 0, MaxDamage = 0, DefenseDistance = 0, Constitution = 15)]
+
+  class ComHybridContainer : AASMAContainer
+  {
+    List<Point> EmptyNeedleLocations = new List<Point>();
+    List<Point> AZNLocations = new List<Point>();
+    List<Point> Navpoints = new List<Point>();
+    List<Point> closePierres = new List<Point>();
+    List<PlanCheckPoint> PlanCheckPointList = new List<PlanCheckPoint>();
+    PlanCheckPoint currentInstruction;
+    bool planIsFinished = true;
+    bool previousInstructionIsFinished = true;
+    enum Desire
+    {
+      None,
+      Unload,
+      Collect
+    };
+
+    Desire goal = Desire.None;
+
+    public override void DoActions()
+    {
+      CheckPerceptions();
+      if (!ReactiveLayer())
+      {
+        if (planIsFinished)
+        {
+          Deliberate();
+          Plan();
+        }
+        else
+        {
+          Execute();
+        }
+
+        if (goal == Desire.None)
+        {
+          MoveRandomly();
+        }
+      }
+    }
+
+    private void CheckPerceptions()
+    {
+      AASMAPlayer framework = this.getAASMAFramework();
+      List<Point> visibleAzn = framework.visibleAznPoints(this);
+      List<Point> visiblePierres = framework.visiblePierres(this);
+      List<Point> visibleHoshimi = framework.visibleHoshimies(this);
+
+      closePierres.Clear();
+
+      foreach (Point n in visibleAzn)
+      {
+        if (!AZNLocations.Contains(n))
+        {
+          AZNLocations.Add(n);
+
+          AASMAMessage msg = new AASMAMessage(this.InternalName, "AZNPoint");
+          msg.Tag = n;
+          getAASMAFramework().sendMessage(msg, "AI");
+
+        }
+      }
+
+      foreach (Point n in visiblePierres)
+      {
+        closePierres.Add(n);
+      }
+
+      foreach (Point n in visibleHoshimi)
+      {
+        AASMAMessage msg = new AASMAMessage(this.InternalName, "HoshimiPoint");
+        msg.Tag = n;
+        getAASMAFramework().sendMessage(msg, "AI");
+      }
+
+    }
+
+    private bool ReactiveLayer()
+    {
+      if (closePierres.Count > 0)
+      {
+        Point closestEnemy = Utils.getNearestPoint(Location, closePierres);
+        int awayVectorX = Location.X - closestEnemy.X;
+        int awayVectorY = Location.Y - closestEnemy.Y;
+
+        Point awayFromPierre = new Point(Location.X + awayVectorX / 2, Location.Y + awayVectorY / 2);
+        this.MoveTo(awayFromPierre);
+
+        return true;
+      }
+
+      return false;
+    }
+
+    private void Deliberate()
+    {
+      if (AZNLocations.Count > 0 && Stock < ContainerCapacity)
+      {
+        goal = Desire.Collect;
+        return;
+      }
+
+      if (Stock > 0 && EmptyNeedleLocations.Count > 0)
+      {
+        goal = Desire.Unload;
+        return;
+      }
+
+      goal = Desire.None;
+
+
+    }
+
+    private void Plan()
+    {
+      PlanCheckPointList.Clear();
+      previousInstructionIsFinished = true;
+      switch (goal)
+      {
+        case Desire.None:
+          PlanCheckPointList.Add(new PlanCheckPoint(Location, PlanCheckPoint.Actions.MoveRandom));
+          break;
+
+        case Desire.Collect:
+          Point closestAZN = Utils.getNearestPoint(Location, AZNLocations);
+          PlanCheckPointList.Add(new PlanCheckPoint(closestAZN, PlanCheckPoint.Actions.Move));
+          PlanCheckPointList.Add(new PlanCheckPoint(closestAZN, PlanCheckPoint.Actions.Collect));
+          planIsFinished = false;
+          break;
+        case Desire.Unload:
+          Point closestNeedle = Utils.getNearestPoint(Location, EmptyNeedleLocations);
+          PlanCheckPointList.Add(new PlanCheckPoint(closestNeedle, PlanCheckPoint.Actions.Move));
+          PlanCheckPointList.Add(new PlanCheckPoint(closestNeedle, PlanCheckPoint.Actions.Unload));
+          planIsFinished = false;
+          break;
+      }
+
+    }
+
+    private void Execute()
+    {
+      if (previousInstructionIsFinished)
+      {
+        if (PlanCheckPointList.Count == 0)
+        {
+          planIsFinished = true;
+          return;
+        }
+        currentInstruction = PlanCheckPointList[0];
+        PlanCheckPointList.RemoveAt(0);
+      }
+
+
+      switch (currentInstruction.action)
+      {
+        case PlanCheckPoint.Actions.Move:
+          if (previousInstructionIsFinished)
+          {
+            MoveTo(currentInstruction.location);
+            previousInstructionIsFinished = false;
+          }
+          else
+          {
+            if (this.Location == currentInstruction.location)
+            {
+              previousInstructionIsFinished = true;
+            }
+          }
+          break;
+
+        case PlanCheckPoint.Actions.MoveRandom:
+          if (previousInstructionIsFinished)
+          {
+            MoveRandomly();
+          }
+          else
+          {
+            previousInstructionIsFinished = true;
+          }
+          break;
+
+        case PlanCheckPoint.Actions.Collect:
+          if (previousInstructionIsFinished)
+          {
+            CollectFrom(currentInstruction.location, 1);
+          }
+          else
+          {
+            if (Stock == ContainerCapacity)
+            {
+              previousInstructionIsFinished = true;
+            }
+          }
+          break;
+
+        case PlanCheckPoint.Actions.Unload:
+          if (previousInstructionIsFinished)
+          {
+            TransferTo(currentInstruction.location, 1);
+          }
+          else
+          {
+            if (this.Location == currentInstruction.location)
+            {
+              previousInstructionIsFinished = true;
+            }
+          }
+          break;
+      }
+
+    }
+
+    private void MoveRandomly()
+    {
+      if (frontClear())
+      {
+        if (Utils.randomValue(100) < 80)
+        {
+          this.MoveForward();
+        }
+        else
+        {
+          this.RandomTurn();
+        }
+      }
+      else
+      {
+        this.RandomTurn();
+      }
+    }
+
+    public override void receiveMessage(AASMAMessage msg)
+    {
+     // getAASMAFramework().logData(this, "received message from " + msg.Sender + " : " + msg.Content);
+
+      string content = msg.Content;
+      Point p = (Point)msg.Tag;
+
+
+      if (content.Equals("AZNPoint"))
+      {
+        if (!AZNLocations.Contains(p))
+        {
+          AZNLocations.Add(p);
+        }
+      }
+
+      if (content.Equals("EmptyNeedle"))
+      {
+        if (!EmptyNeedleLocations.Contains(p))
+        {
+          EmptyNeedleLocations.Add(p);
+        }
+      }
+
+    }
+  }
+}
